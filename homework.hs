536aa45efcc9b940962d9ea4999b725c7d767487
Emanuele <emanuele.carraro.94@gmail.com>
2016-10-25 19:33:15 +0200

    Add few comments

--- homework.hs
+++ homework.hs
@@ -2,17 +2,20 @@ toDigitsPositive n = if n < 10
                       then [n]
                       else toDigitsPositive (n `div` 10) ++ [n `mod` 10]
 
+-- split a number into digits
 toDigits n = if n <= 0 then [] else toDigitsPositive n
 
 toDigitsRev n = reverse (toDigits n)
 
 doubleSecond (x:y:xs) = y*2
 
+-- example: [1,2,3,4] -> [2,2,6,4]
 doubleEveryOther lst = if length lst < 2
                         then lst
                         else doubleEveryOther (take (length lst - 2) lst) ++
                           [doubleSecond (reverse lst), last lst]
 
+-- sum all numbers in a list
 sumDigits [] = 0
 sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
 
@@ -20,6 +23,7 @@ validate n = if (sumDigits (doubleEveryOther (toDigits n)) `mod` 10 == 0)
               then True
               else False
 
+-- hanoi problem (n disks, 3 pegs)
 hanoi n a b c = if n == 1
                   then [(a,b)]
                   else (hanoi (n-1) a c b) ++ [(a,b)] ++ (hanoi (n-1) c b a)
