b1b8d37f89df02c58a886ec2963c66aeb618ba72
Emanuele <emanuele.carraro.94@gmail.com>
2016-10-25 19:36:00 +0200

    Add one comment

--- functionList.hs
+++ functionList.hs
@@ -4,5 +4,7 @@ foldr' f v (x:xs) = f x (foldr' f v xs)
 sum' = foldr' (+) 0
 or' = foldr' (||) False
 length' = foldr' (\_ x -> 1 + x) 0
+
+-- this function converts a binary number into a decimal number
 bitToInt xs = bitToInt' (reverse(xs))
 bitToInt' = foldr' (\a n -> a + n*2) 0
